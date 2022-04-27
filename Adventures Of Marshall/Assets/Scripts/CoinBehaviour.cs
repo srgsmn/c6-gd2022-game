f93cdd6ec36a39f1da99ba907a61e2bc7bd8abf6
Simone Siragusa <sira.simon@icloud.com>
2022-04-27 16:42:20 +0200

    CC + SL animation
    Raffinamento dell'animazione delle CC e SL

--- Adventures Of Marshall/Assets/Scripts/CoinBehaviour.cs
+++ Adventures Of Marshall/Assets/Scripts/CoinBehaviour.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using DG.Tweening;
 
-public class CoinMovement : MonoBehaviour
+public class CoinBehaviour : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        //transform.DORotate(new Vector3(0f, 90f, 0f), 1f, RotateMode.LocalAxisAdd).SetLoops(-1, LoopType.Incremental);
     }
 
     // Update is called once per frame
