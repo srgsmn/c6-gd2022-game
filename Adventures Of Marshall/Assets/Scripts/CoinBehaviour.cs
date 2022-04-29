6224dddd5c422fc912fef1887750e1c722700ff8
Simone Siragusa <sira.simon@icloud.com>
2022-04-29 23:03:58 +0200

    Refining Coins Animation
    Ho animato le monete con uno script universale

--- Adventures Of Marshall/Assets/Scripts/CoinBehaviour.cs
+++ Adventures Of Marshall/Assets/Scripts/CoinBehaviour.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using DG.Tweening;
 
 public class CoinBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Vector3 spinSpeed = Vector3.one;
+    [SerializeField] Vector3 translationSpeed = Vector3.zero;
+
+    private Vector3 positionOnStart;
+
+    private void Start()
     {
-        //transform.DORotate(new Vector3(0f, 90f, 0f), 1f, RotateMode.LocalAxisAdd).SetLoops(-1, LoopType.Incremental);
+        positionOnStart = transform.position;
+        //transform.rotation = Quaternion.Euler(new Vector3(Random.Range(-90f, +90f), Random.Range(-90f, +90f), Random.Range(-90f, +90f)));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(360 * spinSpeed.x * Time.deltaTime, 360 * spinSpeed.y * Time.deltaTime, 360 * spinSpeed.z * Time.deltaTime);
+        transform.position = new Vector3(positionOnStart.x + .125f * Mathf.Sin(translationSpeed.x * Time.time),
+                                         positionOnStart.y + .125f * Mathf.Sin(translationSpeed.y * Time.time),
+                                         positionOnStart.z + .125f * Mathf.Sin(translationSpeed.z * Time.time));
     }
 }
