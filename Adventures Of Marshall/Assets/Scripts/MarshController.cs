1609a43434620dca0417aef04ce2640acbbcd3c4
Simone Siragusa <sira.simon@icloud.com>
2022-05-02 18:59:23 +0200

    Correction of Controller + Enemy Rewarding (Draft)

--- Adventures Of Marshall/Assets/Scripts/MarshController.cs
+++ Adventures Of Marshall/Assets/Scripts/MarshController.cs
@@ -7,11 +7,13 @@ public class MarshController : MonoBehaviour
     [SerializeField] private float speed = 5.0f;
 
     private Rigidbody myRB;
+    [SerializeField] private bool isJumping;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Marshall is now living");
+        isJumping = false;
     }
 
     // Update is called once per frame
@@ -20,43 +22,71 @@ public class MarshController : MonoBehaviour
         myRB = GetComponent<Rigidbody>();
         myRB.isKinematic = false;
 
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(speed * hInput * Time.deltaTime, 0f, speed * vInput * Time.deltaTime);
+
+        /*
+        //if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey("up"))
         {
             Debug.Log("Marsh's moving forward");
-            transform.position += new Vector3(0f, 0f, speed * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            Debug.Log("Marsh's moving backward");
-            transform.position += new Vector3(0f, 0f, -speed * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            Debug.Log("Marsh's moving right");
             transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
         }
 
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            Debug.Log("Marsh's moving left");
+        //if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey("down"))
+            {
+            Debug.Log("Marsh's moving backward");
             transform.position += new Vector3(-speed * Time.deltaTime, 0f, 0f);
         }
 
-        if (Input.GetButtonDown("Jump"))
+        //if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey("right"))
+        {
+            Debug.Log("Marsh's moving right");
+            transform.position += new Vector3(0f, 0f, -speed * Time.deltaTime);
+        }
+
+        //if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey("left"))
+        {
+            Debug.Log("Marsh's moving left");
+            transform.position += new Vector3(0f, 0f, +speed * Time.deltaTime);
+        }
+        */
+
+        if (Input.GetButtonDown("Jump") && !isJumping)
         {
             Debug.Log("\tPreparing to jump");
             transform.localScale += new Vector3(0f, -.25f, 0f);
         }
 
-        if (Input.GetButtonUp("Jump"))
+        if (Input.GetButtonUp("Jump") && !isJumping)
         {
+            isJumping = true;
             Debug.Log("\tNow jumping!!!");
             transform.localScale += new Vector3(0f, .25f, 0f);
             myRB.isKinematic = true;
             transform.position += new Vector3(0f, 2f, 0f);
             myRB.isKinematic = false;
+
+            if (myRB.velocity.y == 0)
+                isJumping = false;
+        }
+
+        /*while (myRB.velocity.y != 0)
+            isJumping = true;
+        */
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy Head"))
+        {
+            Destroy(collision.transform.parent.gameObject);
         }
     }
 
